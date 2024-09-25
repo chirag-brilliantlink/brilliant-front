7431deed275571abde309194b5c9e2ffb5fe6056
akshay-workspace <akshayworkspace77@gmail.com>
2024-09-25 10:11:23 +0530

    header and global css

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,15 +1,41 @@
 import styled from "styled-components";
 const Header = () => {
   return (
-    <Wrapper>Header is here ... dflkjdsfjlk</Wrapper>
-  )
-}
+    <HeaderContainer>
+      <LogoWrapper>
+        <h3>BLS</h3>
+      </LogoWrapper>
+      <MenuWrapper>
+        <Square></Square>
+        <p>Menu</p>
+      </MenuWrapper>
+    </HeaderContainer>
+  );
+};
 export default Header;
-const Wrapper = styled.div`
-  color: green;
-  background-color: #d1f6fa;
-  height: 100px;
-  border: 1px solid black;
-`
-
-
+const HeaderContainer = styled.header`
+  color: white;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+`;
+const LogoWrapper = styled.div`
+  h3 {
+    font-size: var(--font-medium);
+  }
+`;
+const MenuWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  p {
+    font-size: var(--font-medium);
+  }
+`;
+const Square = styled.div`
+  background-color: white;
+  width: 30px;
+  height: 30px;
+`;
