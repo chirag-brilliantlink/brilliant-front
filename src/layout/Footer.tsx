4bd281e977d2b82c816b6d0b07999c8c364f083a
shibaramdas-git <shibaram.daseng@gmail.com>
2024-09-28 10:17:35 +0530

    Updated layout foolder files to PascalCase

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+const Footer = () => {
+  return (
+    <Wrapper>
+      Footer is here ... Bro... dfjkldfljkdf Lorem, ipsum dolor sit amet
+      consectetur adipisicing elit. Consectetur tempore obcaecati qui vel magni
+      earum culpa reprehenderit suscipit voluptatibus. Odit dolorum impedit est
+      praesentium perferendis illum obcaecati alias quibusdam incidunt!
+    </Wrapper>
+  );
+};
+export default Footer;
+const Wrapper = styled.div`
+  color: green;
+  background-color: #fbe3c6;
+  height: 100px;
+  border: 1px solid black;
+`;
---
+++ src/layout/Header.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+const Header = () => {
+  return (
+    <Wrapper>
+      Header is here ... dflkjdsfjlk Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Mollitia autem ducimus recusandae nemo dolore numquam,
+      tempora suscipit dolorem vero magnam!
+    </Wrapper>
+  );
+};
+export default Header;
+const Wrapper = styled.div`
+  color: green;
+  background-color: #d1f6fa;
+  height: 100px;
+  border: 1px solid black;
+`;
